3ef11e844a880f3ce4e80e04151aac22b8855e49
Jean C. Ragusa <jean.ragusa@tamu.edu>
2014-12-17 23:15:32 -0600

    plotting

--- smoothed_heaviside.m
+++ smoothed_heaviside.m
@@ -1,10 +1,10 @@
 clear all; close all; clc;
 
 
-x=-0.2:0.001:0.2;
+x=-0.0:0.001:1;
 
-threshold=0.05;
-delta=0.025;
+threshold=0.15;
+delta=threshold;
 lower_bound=threshold-delta;
 upper_bound=threshold+delta;
 for i=1:length(x);
@@ -18,17 +18,22 @@ for i=1:length(x);
     end
 end
 
-plot(x,y)
+threshold=0.05;
+a=3;
+y_old=0.5*(tanh(a*(x-threshold))+abs(tanh(a*(x-threshold)) ) );
+
+plot(x,y,'-',x,y_old,'r-')
+legend('smoothed heaviside','old function')
        
 
-% function H = Heaviside(phi,epsilon)  
-% %   Heaviside(phi,epsilon)  compute the smooth Heaviside function 
-% %   
-% %   created on 04/26/2004 
-% %   author: Chunming Li 
-% %   email: li_chunming@hotmail.com 
-% %   Copyright (c) 2004-2006 by Chunming Li 
-epsilon=delta/10;
-H = 0.5*(1+ (2/pi)*atan((x-threshold)./epsilon));
-hold all
-plot(x,H)
+% % function H = Heaviside(phi,epsilon)  
+% % %   Heaviside(phi,epsilon)  compute the smooth Heaviside function 
+% % %   
+% % %   created on 04/26/2004 
+% % %   author: Chunming Li 
+% % %   email: li_chunming@hotmail.com 
+% % %   Copyright (c) 2004-2006 by Chunming Li 
+% epsilon=delta/10;
+% H = 0.5*(1+ (2/pi)*atan((x-threshold)./epsilon));
+% hold all
+% plot(x,H)
